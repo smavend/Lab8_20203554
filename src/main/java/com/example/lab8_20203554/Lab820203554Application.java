dbbc1f26f24080dd0d6ac0499f20a1ff44e2e4bb
amanr <amanrique068@gmail.com>
2023-06-13 20:37:19 -0500

    primer commit

---
+++ src/main/java/com/example/lab8_20203554/Lab820203554Application.java
@@ -0,0 +1,13 @@
+package com.example.lab8_20203554;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab820203554Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab820203554Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab8_20203554/Lab820203554ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab8_20203554;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab820203554ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
